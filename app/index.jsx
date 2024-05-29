0ec4a4056cc5cd6d2c310a41585ade27b9497b64
Hanzla Nouman <150622496+Hanzla-Nouman@users.noreply.github.com>
2024-05-28 23:36:54 -0700

    Setting env for movie app in g3

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-neutral-800">
+      <Text className="text-3xl text-white font-bold">Edit to edit this screen.</Text>
+      <Text className="text-3xl  font-bold">Edit to edit this screen.</Text>
+      <Text className="text-3xl  font-bold">Edit to edit this screen.</Text>
+      <Text className="text-3xl  font-bold">Edit to edit this screen.</Text>
+    </View>
+  );
+}
