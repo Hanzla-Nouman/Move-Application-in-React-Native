8ff22facb16b7a3dfdd826ea588b812fcc861edf
Hanzla Nouman <150622496+Hanzla-Nouman@users.noreply.github.com>
2024-05-28 23:39:28 -0700

    Setting env

--- app/index.jsx
+++ app/index.jsx
@@ -4,9 +4,6 @@ export default function Index() {
   return (
     <View className="flex-1 bg-neutral-800">
       <Text className="text-3xl text-white font-bold">Edit to edit this screen.</Text>
-      <Text className="text-3xl  font-bold">Edit to edit this screen.</Text>
-      <Text className="text-3xl  font-bold">Edit to edit this screen.</Text>
-      <Text className="text-3xl  font-bold">Edit to edit this screen.</Text>
     </View>
-  );
+  ); 
 }
